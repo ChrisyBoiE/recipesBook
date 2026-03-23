4b959507ab9d0745fb2bf5a506a0d21853827dca
ChrisyBoiE <krisztiansz0315@gmail.com>
2026-03-23 10:47:56 +0100

    Create the project of the application for recipesBook

---
+++ recipes-book/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <h2>Receptkönyv és Bevásárlólista</h2>
+
+    // lista nézet: jelenjen meg a kép, név, a nehézségi szint (difficulty) és a kalória. EXTRA: lista tetejére egy szűrő, ami kliens oldalon szűri a listát/recepteket.
+    // részletes nézet: kattintásra nyíljon meg a recept. A hozzávalókról készüljön egy lista (ingredients)
+    // interakció: Minden hozzávaló mellett legyen egy check ikon vagyy checkhox. Ha a felhasználó bejelöli-> hozzávaló bekerül egy helyi kosárba (elég a useState vagy Context szintjén tárolni, nem kell DB).
+    // Jelenítse meg egy lebegő gombot (FAB) vagy sávot az alkalmazás alján: "Kiválasztott tételek: X db".
+    // Komponensekre bontás. 
+
+  )
+}
+
+export default App
